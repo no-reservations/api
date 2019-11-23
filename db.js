08d1f86330c023996933524242e37388fbe38148
samgomena <samiam4612@gmail.com>
2019-11-23 11:49:57 -0800

    feat: Add db controller

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const database_url = process.env.DB_URL || `mongodb://localhost/test`;
+mongoose.connect(database_url, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+module.exports = db;
