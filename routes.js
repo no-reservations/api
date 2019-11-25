5dcad75a773604917c4c9732239c40a95090aea7
Sam Gomena <samgomena@users.noreply.github.com>
2019-11-25 09:08:33 -0800

    refac: Update route names for restaurants.

--- routes.js
+++ routes.js
@@ -2,12 +2,11 @@ const restaurant = require("./controllers/restaurant");
 const reservation = require("./controllers/reservation");
 
 module.exports = function routes(app) {
-
-    // app.get("/restaurant/all");
-    app.get("/restaurant/:restaurant", restaurant.view);
-    app.post("/restaurant", restaurant.create);
-    app.put("/restaurant");
-    app.delete("/restaurant", restaurant.delete_one);
+    
+    app.get("/restaurants/:restaurant", restaurant.view);
+    app.post("/restaurants/new", restaurant.create);
+    app.put("/restaurants/update");
+    app.delete("/restaurants/remove", restaurant.delete_one);
 
     app.get("/reservation");
     app.post("/reservation");
