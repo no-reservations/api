4ba5e7c65775a33a6bb6cecc5df83295ecb740e8
samgomena <samiam4612@gmail.com>
2019-11-27 14:42:15 -0800

    feat: Add simply validator for phone

--- models/guest.model.js
+++ models/guest.model.js
@@ -3,7 +3,19 @@ const Schema = mongoose.Schema;
 
 const guestModel = new Schema({
     name: { type: String, required: [true, "Guest name is required!"] },
-    phone: { type: String },
+    // Add validator for phone number.
+    // Note: This fails for int'l stuff
+    // See: https://mongoosejs.com/docs/validation.html#custom-validators
+    phone: {
+        type: String,
+        validate: {
+            validator: v => {
+                Promise.resolve(/\d{3}-\d{3}-\d{4}/.test(v));
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    },
     reservations: [{ type: Schema.Types.ObjectId, ref: "Reservation" }],
 });
 
