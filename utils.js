156c6a475776d01e5e9da0035102c045c7759dc2
samgomena <samiam4612@gmail.com>
2019-11-27 14:50:18 -0800

    feat: Add 'validator' like function in utils for adding name values

--- utils.js
+++ utils.js
@@ -1,7 +1,5 @@
 function normalize_name(name) {
-    const lower_name = name.toLowerCase();
-    const sanitized_name = lower_name.replace(/[ -,]/g, "_");
-    return sanitized_name;
+    return name.replace(/[ -,]/g, "_");
 }
 
 module.exports = {
