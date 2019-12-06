fd7b98aa0b558451f3bf02c72542188cf1558083
samgomena <samiam4612@gmail.com>
2019-12-05 17:38:02 -0800

    refac: Remove guest from reservation model in favor of name

--- models/reservation.model.js
+++ models/reservation.model.js
@@ -5,6 +5,7 @@ const reservationModel = new Schema({
     start: { type: Date, required: [true, "Start time is required!"] },
     end: { type: Date, required: [true, "End time is required"] },
     restaurant: { type: Schema.Types.ObjectId, ref: "Restaurant", required: [true, "Restaurant for reservation is required!"] },
+    name: { type: String, required: [true, "Name for reservation is required!"] },
     // Guest: { type: Schema.Types.ObjectId, ref: "Guest", required: [true, "Guest for reservation is required!"] },
     size: { type: Number, required: [true, "Party size is required!"], min: 0 },
 });
