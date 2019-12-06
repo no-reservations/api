7f7864eead4db4d3411e70c64a361b9fa6681e4f
samgomena <samiam4612@gmail.com>
2019-12-05 22:07:02 -0800

    feat: Run CORS * when in debug/dev mode

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ const log_level = DEBUG ? "combined" : "dev";
 app.use(logger(log_level));
 
 // TODO: Specify stricter cors mode in dev environment
+DEBUG && 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
